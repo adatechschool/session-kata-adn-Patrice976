b7bb7274719ac9ccd4203fcab41f04a35d0bd3ee
Patrice976 <catinatp@gmail.com>
2024-12-06 10:34:18 +0100

    construction et vérification de la logique pour deécouper et injecter des éléments

--- adn.js
+++ adn.js
@@ -1,4 +1,23 @@
 //récupération dans le code du contenu du fichier avec tout les nucléotides 
 import { nucleotides } from "./nucleotidesList.js";
 
-console.log(nucleotides)
+//tout les trois éléments 
+
+let proteinCode = [nucleotides[0],nucleotides[1],nucleotides[2]]
+
+
+let nucleotidesArray = []
+
+nucleotidesArray.push(proteinCode)
+
+console.log(nucleotidesArray)
+
+proteinCode = [nucleotides[3],nucleotides[4],nucleotides[5]]
+
+nucleotidesArray.push(proteinCode)
+
+console.log(nucleotidesArray)
+
+
+
+//Crée un tableau avec ces dernier à l'intérieu 
